a1ca5d370655a324cf37dc48b67f94805b8a405b
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-31 11:48:55 +0100

    31/10/2023 11:48

--- mazmorraInterfacesIS/src/main/java/ui/Juego.java
+++ mazmorraInterfacesIS/src/main/java/ui/Juego.java
@@ -1,19 +1,37 @@
 package ui;
 
+import java.io.File;
+
 import javax.swing.*;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
 
 public class Juego {
 
     JFrame framePrincipal = new JFrame("Prueba");
     JButton botonPrueba = new JButton("Prueba");
+    File fileXML = new File("data/mazmorra.xml");
+    private DefaultMutableTreeNode root;
+    private DefaultTreeModel treeModel;
+    JTree fileTree = new JTree();
 
 
     public void Prueba() {
-        botonPrueba.setBounds(130, 100, 100, 40);
-        framePrincipal.add(botonPrueba);
 
-        framePrincipal.setSize(400, 500);
-        framePrincipal.setLayout(null);
+
+    }
+
+    public void pruebaTree(){
+        framePrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        root = new DefaultMutableTreeNode(fileXML);
+        treeModel = new DefaultTreeModel(root);
+
+        fileTree = new JTree(root);
+        fileTree.setShowsRootHandles(true);
+
         framePrincipal.setVisible(true);
+        framePrincipal.setSize(640, 480);
     }
 }
