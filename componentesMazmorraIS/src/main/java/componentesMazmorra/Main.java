4fd55e4c0e3315b93368e241993317fc1f2e3d8c
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-11-28 10:16:58 +0100

    28/11/2023 10:16

--- componentesMazmorraIS/src/main/java/componentesMazmorra/Main.java
+++ componentesMazmorraIS/src/main/java/componentesMazmorra/Main.java
@@ -1,9 +1,11 @@
 package componentesMazmorra;
 
-public class Main {
+import javax.swing.*;
 
+
+public class Main {
     public static void main(String[] args) {
+        JFrame framePrincipal = new JFrame("Mazmorra Frankenstein");
 
     }
-
 }
